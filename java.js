3fcfb51d67f28f08beb6cdd11a6ee91fa3a01597
bahdinizo <t.barbara.dutradinizo@gmail.com>
2022-11-27 15:20:47 -0300

    interacao

--- java.js
+++ java.js
@@ -1,6 +1,10 @@
-var idade = prompt("Digite sua idade")
-if (idade > 22) {
+var x = prompt("Digite sua idade")
+if (x > 22) {
     alert("Você nasceu antes da virada do século")
 } else { 
     alert ("Você nasceu depois da virada do século")
 }
+var y = prompt("Agora, digite a idade da sua mãe");
+        var resultado = parseInt (x) + parseInt(y);
+        var resultado = parseInt (resultado/2);       
+    window.alert("A média da sua idade com a idade da sua mãe é" + " " + resultado);
